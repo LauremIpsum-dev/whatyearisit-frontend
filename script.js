c6efa7b6a88eacca5d79706c5038e1b32a9f00d2
Laurem <karinelauremartins@gmail.com>
2025-06-27 15:15:41 +0200

    frontend

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let urlVercel ="whatyearisit-backend-eight-tau.vercel.app";
 
 fetch(urlVercel)
-.then(response =>response.json());
+.then(response =>response.json())
 .then(data => {data.year
     document.querySelector('#year').textContent = data.year
 });
